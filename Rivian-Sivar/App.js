49f39ae58735df7011ced3dcbcbdead80391ded9
Elizabeth186 <eliizabethsuarezz@gmail.com>
2022-03-23 22:38:43 -0600

    Splash

--- Rivian-Sivar/App.js
+++ Rivian-Sivar/App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
+
+function Splas({navigation}){
+ 
+    setTimeout(()=>{
+       navigation.navigate('Home')
+            }, 3000);
+
+  
+return(
+  <View style={{ alignItems:'center'}}>
+      <Image  
+       style={{marginTop:'90%', width: 210, height: 140, marginBottom: 15 }}
+        source={require("./assets/Logo.png") }/>
+
+    
+  </View>
+)
+
+}
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen  name="splash" options={{headerShown: false}} component={Splas} />
+        <Stack.Screen name="Home" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -24,4 +43,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
