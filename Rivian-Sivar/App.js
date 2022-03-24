5ef85e34a690155aca469fdf0ba205953721b266
Elizabeth186 <eliizabethsuarezz@gmail.com>
2022-03-24 10:49:09 -0600

    Splash & Navegacion

--- Rivian-Sivar/App.js
+++ Rivian-Sivar/App.js
@@ -6,5 +6,40 @@ import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 
 
+function Splas({navigation}){
+
+  setTimeout(()=>{
+     navigation.navigate('Home')
+          }, 3000);
+return(
+<View style={{ alignItems:'center'}}>
+    <Image  
+     style={{marginTop:'90%', width: 210, height: 140, marginBottom: 15 }}
+      source={require("./assets/Logo.png") }/>
+</View>
+)
+
+}
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+return (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen  name="splash" options={{headerShown: false}} component={Splas} />
+      <Stack.Screen name="Home" component={LoginScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+});
 
 
