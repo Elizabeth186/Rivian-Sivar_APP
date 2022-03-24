40579486c9945761e137b11dba1f814ef7c0f2f4
Elizabeth186 <eliizabethsuarezz@gmail.com>
2022-03-24 11:20:10 -0600

    App, Splash & navegacion

--- Rivian-Sivar/App.js
+++ Rivian-Sivar/App.js
@@ -6,40 +6,42 @@ import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 
 
-function splash({navigation}){
+function Splash({navigation}){
 
-  setTimeout(()=>{
-     navigation.navigate('Home')
-          }, 3000);
+    setTimeout(()=>{
+       navigation.navigate('Home')
+            }, 3000);
+
+
 return(
-<View style={{ alignItems:'center'}}>
-    <Image  
-     style={{marginTop:'90%', width: 210, height: 140, marginBottom: 15 }}
-      source={require("./assets/Logo.png") }/>
-</View>
+  <View style={{ alignItems:'center'}}>
+      <Image  
+       style={{marginTop:'90%', width: 210, height: 140, marginBottom: 15 }}
+        source={require("./assets/Logo.png") }/>
+
+
+  </View>
 )
 
 }
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen  name="splash" options={{headerShown: false}} component={Splash} />
-      <Stack.Screen name="Home" component={LoginScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen  name="splash" options={{headerShown: false}} component={Splash} />
+        <Stack.Screen name="Home" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-
